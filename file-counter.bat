09a296d9ba43c04987b38d1e8a96df18181ca466
ccatura <charlie@charliecatura.com>
2023-04-14 11:04:57 -0400

    tweaked order of code

--- file-counter.bat
+++ file-counter.bat
@@ -5,16 +5,17 @@ set /A prev=0
 :COUNT
 for /f %%A in ('dir "G:\My Drive\JTV\Work\*.jpg" ^| find "File(s)"') do set /A cnt=%%A
 
+cls
+echo File count = %cnt%
+
 if %prev% NEQ %cnt% (
     color f0
     ping 127.0.0.1 -n 2 > nul
     color 0f
     set /A prev=%cnt%
 )
-cls
 
-echo File count = %cnt%
-ping 127.0.0.1 -n 3 > nul
+ping 127.0.0.1 -n 4 > nul
 goto :count
 
 pause
