25fc45278ad5032f8f0cacb114506a04fc71e42d
ccatura <charlie@charliecatura.com>
2023-04-26 11:43:50 -0400

    latest version changes

--- live-file-counter.bat
+++ live-file-counter.bat
@@ -4,14 +4,17 @@ set /P goal=Goal quantity?
 set /A prev=0
 
 :COUNT
-for /f %%A in ('dir "G:\My Drive\JTV\Work\*.jpg" ^| find "File(s)"') do set /A cnt=%%A
+for /f %%A in ('dir "G:\My Drive\JTV\Work\*.*" ^| find "File(s)"') do set /A cnt=%%A
 
-cls
+
 set /A diff=%goal%-%cnt%
-echo %diff% more shots until goal of %goal%
-echo %cnt% images
-
 if %prev% NEQ %cnt% (
+    cls
+    echo %diff% more shots until goal of %goal%.
+    echo|set /p= Images shot today: 
+    forfiles /P "G:\My Drive\JTV\Work" /M "*" /D +0 /C "cmd /C if @isdir==FALSE echo _" | find /C "_"
+    echo Total images     : %cnt%
+
     color f0
     ping 127.0.0.1 -n 1 > nul
     color 0f
@@ -22,7 +25,7 @@ if %prev% NEQ %cnt% (
     set /A prev=%cnt%
 )
 
-ping 127.0.0.1 -n 4 > nul
+ping 127.0.0.1 -n 3 > nul
 goto :count
 
 pause
